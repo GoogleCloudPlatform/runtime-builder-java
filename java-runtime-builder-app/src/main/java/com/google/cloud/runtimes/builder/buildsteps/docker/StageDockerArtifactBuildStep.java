ff13e9b484aa0946f98157ade1c7ab909f868bb4
Alex Sloan <alexsloan@google.com>
2017-03-22 13:46:46 -0400

    Just copy the .dockerignore to staging directory

--- java-runtime-builder-app/src/main/java/com/google/cloud/runtimes/builder/buildsteps/docker/StageDockerArtifactBuildStep.java
+++ java-runtime-builder-app/src/main/java/com/google/cloud/runtimes/builder/buildsteps/docker/StageDockerArtifactBuildStep.java
@@ -26,19 +26,14 @@ import com.google.inject.Inject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.PathMatcher;
-import java.nio.file.Paths;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class StageDockerArtifactBuildStep extends BuildStep {
 
@@ -122,53 +117,20 @@ public class StageDockerArtifactBuildStep extends BuildStep {
       throw new IllegalArgumentException(String.format("%s is not a valid directory.", directory));
     }
 
-    Stream<Path> fileStream = Files.list(directory)
+    List<Path> validArtifacts = Files.list(directory)
         // filter out files that don't end in .war or .jar
         .filter((path) -> {
           String extension = com.google.common.io.Files.getFileExtension(path.toString());
           return extension.equals("war") || extension.equals("jar");
-        });
+        })
+        .collect(Collectors.toList());
 
-    // if a dockerignore file exists, filter based on its contents
-    Path dockerIgnoreFile = directory.resolve(".dockerignore");
-    if (Files.exists(dockerIgnoreFile)) {
-      PathMatcher dockerIgnoreMatcher = getDockerIgnoreFileMatcher(dockerIgnoreFile);
-      fileStream = fileStream
-          // apply the dockerignore filter to all file names relative to the workspace root
-          .filter((path) -> {
-            Path relativePath = path.subpath(directory.getNameCount(), path.getNameCount());
-            return !dockerIgnoreMatcher.matches(relativePath);
-          });
-    }
-
-    List<Path> validArtifacts = fileStream.collect(Collectors.toList());
     if (validArtifacts.size() < 1) {
       throw new ArtifactNotFoundException();
     } else if (validArtifacts.size() > 1) {
       throw new TooManyArtifactsException(validArtifacts);
     } else {
       return validArtifacts.get(0);
-    }
-  }
-
-  private PathMatcher getDockerIgnoreFileMatcher(Path file) throws IOException {
-    try (BufferedReader in = Files.newBufferedReader(file)) {
-      StringBuilder pathMatcherExpression = new StringBuilder("glob:");
-      in.lines()
-          // strip leading and trailing whitespace
-          .map((line) -> line.replaceAll("^\\s*", "").replaceAll("\\s*$", ""))
-          // clean "." and ".." elements from the path, etc.
-          .map((line) -> Paths.get(line).normalize().toString())
-          // remove comments and blank lines
-          .filter((line) -> !line.isEmpty() && line.charAt(0) != '#')
-          // add all remaining lines to the matcher
-          // TODO delimiter required between elements?
-          .forEach(pathMatcherExpression::append);
-
-      // TODO handle case where no lines are appended
-
-      // TODO identify explicit includes (lines starting with !)
-      return FileSystems.getDefault().getPathMatcher(pathMatcherExpression.toString());
     }
   }
 
--- java-runtime-builder-app/src/test/java/com/google/cloud/runtimes/builder/buildsteps/docker/StageDockerArtifactBuildStepTest.java
+++ java-runtime-builder-app/src/test/java/com/google/cloud/runtimes/builder/buildsteps/docker/StageDockerArtifactBuildStepTest.java
@@ -37,7 +37,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Optional;
 
 /**
  * Unit tests for {@link StageDockerArtifactBuildStep}.
@@ -88,31 +87,13 @@ public class StageDockerArtifactBuildStepTest {
     String dockerIgoreContents = "# ignore files that start with 'ignored_*'\n"
         + "ignored_*\n";
     Path workspace = new TestWorkspaceBuilder()
-        .file("notignored.jar").build()
-        .file("ignored_artifact.war").build()
-        .file("ignored_other_artifact.jar").build()
+        .file("default.jar").build()
         .file(".dockerignore").withContents(dockerIgoreContents).build()
         .build();
 
     initBuildStep(null).doBuild(workspace, metadata);
-    assertTrue(Files.exists(getDockerStagingDir(workspace).resolve("notignored.jar")));
-  }
-
-  @Test(expected = BuildStepException.class)
-  public void testDoBuild_withDockerIgnoreAndArtifactPathOverride() throws BuildStepException,
-      IOException {
-    String artifactPathOverridden = "some_dir/my_artifact.jar";
-    String dockerIgnoreContents = "# comment \n"
-        + "some_dir/**";
-
-    Path workspace = new TestWorkspaceBuilder()
-        .file("default.jar").build()
-        .file(artifactPathOverridden).build()
-        .file(".dockerignore").withContents(dockerIgnoreContents).build()
-        .build();
-
-    // should fail, since the config is pointing to an artifact that's being explicitly ignored
-    initBuildStep(null).doBuild(workspace, metadata);
+    assertTrue(Files.exists(getDockerStagingDir(workspace).resolve("default.jar")));
+    assertTrue(Files.exists(getDockerStagingDir(workspace).resolve(".dockerignore")));
   }
 
   @Test

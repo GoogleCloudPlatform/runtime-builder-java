b0c7a95d4a8daba6d79e2c8cfd137bb614fa5491
Alex Sloan <alexsloan@google.com>
2017-04-18 12:02:28 -0400

    Script for building and running from head (#26)

--- DEVELOPING.md
+++ DEVELOPING.md
@@ -2,12 +2,13 @@
 
 This document contains instructions on how to build and test this pipeline.
 
-## Local Build
+## Building
+### Local Build
 To build all images for this pipeline locally, you need docker and maven installed. Running 
 `mvn install` will compile all java sources and build all docker images. After the build is complete, 
 all resulting images will be present in your local docker repository.
 
-## Cloud Build
+### Cloud Build
 Google Cloud Container Builder can also be used to build images in the pipeline. The 
 [Google Cloud SDK](https://cloud.google.com/sdk/) must be installed locally in order to use Google
 Cloud Container Builder. We provide a script to make it easy to build using Container Builder:
@@ -17,4 +18,13 @@ Cloud Container Builder. We provide a script to make it easy to build using Cont
 PROJECT_ID=my-gcp-project
 TAG=tag
 ./scripts/build.sh gcr.io/$PROJECT_ID $TAG
+```
+
+## Running
+The [run-pipeline](scripts/run-pipeline.sh) script can be used to build and run the pipeline. It 
+builds all build steps locally, pushes to GCR, and executes the build pipeline via Google Cloud 
+Container Builder. The script uses the default GCP project that is configured in your Cloud SDK
+settings.
+```bash
+./scripts/run-pipeline.sh /path/to/some/source/to/build
 ```
---
+++ scripts/run-pipeline.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# Copyright 2016 Google Inc. All rights reserved.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.bash
+
+# Assembles and runs the build pipeline on a local source direcory
+set -e
+
+DIR=$(dirname $0)
+PROJECT_ROOT=$DIR/..
+GCLOUD_PROJECT=$(gcloud config get-value project 2> /dev/null)
+
+SOURCE_DIR=$1
+OUTPUT_IMAGE=$2
+
+if [ -z $SOURCE_DIR ]; then
+  echo "Usage: $0 <source_directory> [output_image_name]"
+  exit 1;
+fi
+
+if [ -z $OUTPUT_IMAGE ]; then
+  OUTPUT_IMAGE=gcr.io/$GCLOUD_PROJECT/output-image
+fi
+
+mvn install
+
+# retag and push the built image
+BUILDER_IMAGE="gcr.io/$GCLOUD_PROJECT/runtime-builder:$(date -u +%Y-%m-%d_%H_%M)"
+docker tag runtime-builder $BUILDER_IMAGE
+gcloud docker -- push $BUILDER_IMAGE
+
+# escape special characters in the builder image string so we can use it as a sed substitution below
+ESCAPED_BUILDER_IMAGE=$(echo $BUILDER_IMAGE | sed -e 's/[\/&]/\\&/g')
+
+# prepare the build pipeline config file, pointing to the built image
+PIPELINE_CONFIG=$PROJECT_ROOT/target/java_temlated.yaml
+sed -e "s/gcr.io\/gcp-runtimes\/java\/runtime-builder\:latest/$ESCAPED_BUILDER_IMAGE/" $PROJECT_ROOT/java.yaml > $PIPELINE_CONFIG
+
+gcloud container builds submit $SOURCE_DIR \
+  --config $PIPELINE_CONFIG \
+  --substitutions "_OUTPUT_IMAGE=$OUTPUT_IMAGE"
+

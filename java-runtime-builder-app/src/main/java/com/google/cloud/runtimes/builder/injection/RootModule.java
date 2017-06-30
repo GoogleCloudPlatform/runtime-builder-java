7a497426bbafd6c63483536107e34a175380352f
Alex Sloan <alexsloan@google.com>
2017-06-30 12:29:12 -0400

    Update format of runtime map arguments (#59)

--- java-runtime-builder-app/src/main/java/com/google/cloud/runtimes/builder/injection/RootModule.java
+++ java-runtime-builder-app/src/main/java/com/google/cloud/runtimes/builder/injection/RootModule.java
@@ -23,34 +23,37 @@ import com.google.cloud.runtimes.builder.config.AppYamlParser;
 import com.google.cloud.runtimes.builder.config.YamlParser;
 import com.google.cloud.runtimes.builder.config.domain.AppYaml;
 import com.google.cloud.runtimes.builder.config.domain.JdkServerLookup;
+import com.google.common.base.Preconditions;
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
 import com.google.inject.TypeLiteral;
 import com.google.inject.assistedinject.FactoryModuleBuilder;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * Module class for configuring Guice bindings.
  */
 public class RootModule extends AbstractModule {
 
-  private final String jdkMapArg;
-  private final String serverMapArg;
+  private final String[] jdkMappings;
+  private final String[] serverMappings;
 
   /**
    * Constructs a new {@link RootModule} for Guice.
    *
-   * @param jdkMapArg a JSON object that maps jdk names to docker images
-   * @param serverMapArg a JSON object that maps jdk and server names to docker images
+   * @param jdkMappings mappings between supported jdk versions and docker images
+   * @param serverMappings mappings between supported jdk versions, server types, and docker images
    */
-  public RootModule(String jdkMapArg, String serverMapArg) {
-    this.jdkMapArg = jdkMapArg;
-    this.serverMapArg = serverMapArg;
+  public RootModule(String[] jdkMappings, String[] serverMappings) {
+    Preconditions.checkNotNull(jdkMappings);
+    Preconditions.checkNotNull(serverMappings);
+
+    this.jdkMappings = jdkMappings;
+    this.serverMappings = serverMappings;
   }
 
   @Override
@@ -66,13 +69,25 @@ public class RootModule extends AbstractModule {
 
   @Provides
   protected JdkServerLookup provideJdkServerLookup() throws IOException {
-    ObjectMapper objectMapper = new ObjectMapper();
-    Map<String, String> jdkMap  = objectMapper.readValue(jdkMapArg,
-        new TypeReference<Map<String, String>>(){});
-    Map<String, String> serverMap  = objectMapper.readValue(serverMapArg,
-        new TypeReference<Map<String, String>>(){});
+    return new JdkServerLookup(buildMap(jdkMappings), buildMap(serverMappings));
+  }
 
-    return new JdkServerLookup(jdkMap, serverMap);
+  /*
+   * Converts an array of mapping strings, with keys and values separated by an '=' character, into
+   * a Map<String,String>.
+   */
+  private static Map<String, String> buildMap(String[] mappings) {
+    return Arrays.stream(mappings)
+        .map(s -> {
+          String[] split = s.split("=");
+          // make sure mappings are formatted correctly
+          if (split.length != 2)  {
+            throw new IllegalArgumentException("Invalid mapping: '" + s + "'. "
+                + "All jdk/server mappings must be formatted as: KEY=VAL");
+          }
+          return split;
+        })
+        .collect(Collectors.toMap(a -> a[0], a -> a[1]));
   }
 
 }

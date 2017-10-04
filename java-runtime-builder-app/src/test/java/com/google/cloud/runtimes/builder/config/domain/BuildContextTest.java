c834359a09801a713fcba9f53b391674915e5d12
ChengyuanZhao <cccz@google.com>
2017-10-04 13:10:32 -0400

    removed unused import

--- java-runtime-builder-app/src/test/java/com/google/cloud/runtimes/builder/config/domain/BuildContextTest.java
+++ java-runtime-builder-app/src/test/java/com/google/cloud/runtimes/builder/config/domain/BuildContextTest.java
@@ -3,7 +3,6 @@ package com.google.cloud.runtimes.builder.config.domain;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
 import com.google.cloud.runtimes.builder.TestUtils.TestWorkspaceBuilder;
 import java.io.FileNotFoundException;

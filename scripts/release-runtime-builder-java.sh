8a3edbdb8744b1ed10a4b192e472b783578b85e7
Don McCasland <donmccasland@google.com>
2019-04-04 15:04:31 -0700

    fixing release script path

--- scripts/release-runtime-builder-java.sh
+++ scripts/release-runtime-builder-java.sh
@@ -2,6 +2,6 @@
 export KOKORO_GITHUB_DIR=${KOKORO_ROOT}/src/github
 source ${KOKORO_GFILE_DIR}/kokoro/common.sh
 
-cd ${KOKORO_GITHUB_DIR}
+cd ${KOKORO_GITHUB_DIR}/runtime-builder-java
 
 ./scripts/build.sh ${DOCKER_NAMESPACE} ${TAG}
